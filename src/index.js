f9e117bf18c7059a0159db27de9fbfb5178d82b5
Lucas Zordan <lucaszordan0204@gmail.com>
2022-02-01 13:14:27 -0300

    implementing the features

--- src/index.js
+++ src/index.js
@@ -10,19 +10,95 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found!'
+    });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) {
+    return next();
+  } else if (user.todos.length < 10) {
+    return next();
+  }
+
+  return response.status(403).json({
+    error: 'Update to Pro to create more todos!'
+  });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  if (!username) {
+    return response.status(401).json({
+      error: 'Username must be provided!'
+    });
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found!'
+    });
+  }
+
+  const idIsValid = validate(id);
+
+  if (!idIsValid) {
+    return response.status(400).json({
+      error: 'Invalid id!'
+    });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Todo not found!'
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  if (!id) {
+    return response.status(400).json({
+      error: 'Id must be provided!'
+    });
+  }
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found!'
+    });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
